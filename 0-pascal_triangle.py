6b08c82702beb211ac9bf068041bc3300f764886
Githu77 <githubenard16@gmail.com>
2024-05-30 13:49:50 +0300

    add files README.md and 0-pascal_triangle.py

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+def pascal_triangle(n):
+    """
+         returns integers lists of Pascal’s triangle of n
+         Returns nothing if n <= 0
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for x in range(1, n):
+        row = [1]
+        for y in range(1, x):
+            row.append(triangle[x - 1][y - 1] + triangle[x - 1][y])
+        row.append(1)
+        triangle.append(row)
+    return triangle

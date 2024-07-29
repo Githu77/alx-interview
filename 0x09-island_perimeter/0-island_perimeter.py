33cec8b65b33d9927c5fbe8cadab6ef0cfd32179
Githu77 <githubenard16@gmail.com>
2024-07-29 16:02:47 +0300

    add file

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""to get island Perimeter"""
+
+def island_perimeter(grid):
+    """returns islands perimeter"""
+    area = 0
+    for row in grid + list(map(list, zip(*grid))):
+        for i1, i2 in zip([0] + row, row + [0]):
+            area += int(i1 != i2)
+    return area
